1256966260e0409afaa9f0f35d70ded01dcc8e46
jdkizer9 <james.kizer@gmail.com>
2017-01-09 14:48:24 -0500

    Initial commit

---
+++ Example/ResearchSuiteTaskBuilder/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ResearchSuiteTaskBuilder
+//
+//  Created by jdkizer9 on 01/09/2017.
+//  Copyright (c) 2017 jdkizer9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
