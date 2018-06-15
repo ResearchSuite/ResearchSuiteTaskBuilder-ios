fb7fdaaadfd42851b00db073b1a97b6605136f86
jdkizer9 <jdkizer9@users.noreply.github.com>
2018-06-14 23:01:57 -0400

    Added countdown step support

---
+++ ResearchSuiteTaskBuilder/Classes/Step Providers/Generators/RSTBCountdownStepGenerator.swift
@@ -0,0 +1,45 @@
+//
+//  RSTBCountdownStepGenerator.swift
+//  ResearchSuiteTaskBuilder
+//
+//  Created by James Kizer on 6/14/18.
+//
+
+import UIKit
+
+import ResearchKit
+import Gloss
+
+open class RSTBCountdownStepGenerator: RSTBBaseStepGenerator {
+    
+    public init(){}
+    
+    open var supportedTypes: [String]! {
+        return ["countdown"]
+    }
+    
+    open func generateStep(type: String, jsonObject: JSON, helper: RSTBTaskBuilderHelper) -> ORKStep? {
+        
+        guard let stepDescriptor = RSTBStepDescriptor(json:jsonObject) else {
+            return nil
+        }
+        
+        let step = ORKCountdownStep(identifier: stepDescriptor.identifier)
+        if let duration: TimeInterval = "duration" <~~ jsonObject {
+            step.stepDuration = duration
+        }
+        step.title = stepDescriptor.title
+        step.text = stepDescriptor.text
+        step.isOptional = stepDescriptor.optional
+        
+        return step
+    }
+    
+    open func processStepResult(type: String,
+                                jsonObject: JsonObject,
+                                result: ORKStepResult,
+                                helper: RSTBTaskBuilderHelper) -> JSON? {
+        return nil
+    }
+    
+}
